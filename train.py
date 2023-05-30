49440b1bc35f04263428f1ce2b99ec0ef64335b8
Carl Winge <carlewinge@gmail.com>
2023-05-30 14:28:04 -0500

    update

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from agents import peract_bc
 import os
+from helpers.utils import create_obs_config
+from yarr.replay_buffer.wrappers.pytorch_replay_buffer import PyTorchReplayBuffer
 
 @hydra.main(version_base=None, config_path='conf', config_name='config')
 def main(cfg: DictConfig) -> None:
@@ -16,6 +18,7 @@ def main(cfg: DictConfig) -> None:
                                    cfg.method.name)
 
     task = cfg.rlbench.tasks[0]
+    tasks = cfg.rlbench.tasks
     task_folder = task # if not multi_task else 'multi'
     replay_path = os.path.join(cfg.replay.path, task_folder, cfg.method.name, 'seed%d' % 0)
 
@@ -28,7 +31,7 @@ def main(cfg: DictConfig) -> None:
         cfg.method.voxel_sizes,
         cfg.rlbench.camera_resolution
     )
-
+    rank = 0
     peract_bc.launch_utils.fill_multi_task_replay(
         cfg,
         obs_config,
@@ -46,6 +49,11 @@ def main(cfg: DictConfig) -> None:
         cfg.method.crop_augmentation,
         keypoint_method=cfg.method.keypoint_method
     )
+
+    agent = peract_bc.launch_utils.create_agent(cfg)
+
+    wrapped_replay = PyTorchReplayBuffer(replay_buffer, num_workers=cfg.framework.num_workers)
+
 
 if __name__ == "__main__":
     main()
